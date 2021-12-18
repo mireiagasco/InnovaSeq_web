a41bcdab81ced1df36a39a60ffaefa1e6d33ce66
Mireia Gasco Agorreta <63343593+mireiagasco@users.noreply.github.com>
2021-12-18 12:47:15 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-$(".nav-link").on("click", function(){
+$(".nav-link").on("click", function () {
     $(".nav-link.active").removeClass("active");
     $(this).addClass("active");
- });
+});
+
+$(window).scroll(function () {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('.top-button').addClass("visible");
+    } else {
+        $('.top-button').removeClass("visible");
+    }
+});
+
